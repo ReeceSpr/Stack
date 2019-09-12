6234a2f1629bbfa9c22dcb29e7d6acaadb43c54f
Reece <reece.spragg@gmail.com>
2019-09-12 20:55:34 +1200

    Added Splash Screen

--- app/src/main/java/com/boltx/stack/MainActivity.java
+++ app/src/main/java/com/boltx/stack/MainActivity.java
@@ -1,8 +1,18 @@
 package com.boltx.stack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.animation.PathInterpolatorCompat;
 
 import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Scene;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animate();
+    }
+
+    void animate(){
+        ImageView logo = findViewById(R.id.imageView);
+        Fade mFade;
+        ViewGroup rootView;
+
+        // Get the root view and create a transition
+        rootView = (ViewGroup) findViewById(R.id.main_layout);
+        mFade = new Fade(Fade.OUT);
+
+        //Add easing
+        Interpolator customInterpolator = PathInterpolatorCompat.create((float) 0.420, (float) 0.000, (float) 0.580, (float) 1.000);
+        mFade.setInterpolator(customInterpolator);
+
+        TransitionManager.beginDelayedTransition(rootView, mFade);
+
+        logo.animate().translationY(2000).setDuration(1000);
     }
 }
---
+++ app/src/main/java/com/boltx/stack/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.boltx.stack;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.RotateDrawable;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.transition.Fade;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.animation.PathInterpolatorCompat;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        animate();
+        SystemClock.sleep(5000);
+        finish();
+    }
+    private void animate() {
+    }
+}
